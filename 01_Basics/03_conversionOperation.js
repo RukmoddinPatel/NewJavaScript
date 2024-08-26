9a4b5be3462e0f37d79588b1e844f799f5efff1a
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-08-26 23:29:25 +0530

    Conversion Operations on Data Types

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,107 @@
+let name="rukmoddin";  // String
+let number1="22"       // String
+let number2="22a"      // String
+let number3=true;      // Boolean
+let number4=22;        //Number 
+
+
+// 1.Converting into the number 
+console.log("1.Converting into the Number");
+
+
+console.log(Number(name));        //NaN => Not a Number 
+console.log(Number(number1));     //22
+console.log(Number(number2));     //NaN
+console.log(Number(number3));     // 1
+console.log(Number(number4));     // 22
+
+
+
+console.log(typeof(Number(name)));
+console.log(typeof(Number(number1)));
+console.log(typeof(Number(number2)));
+console.log(typeof(Number(number3)));
+console.log(typeof(Number(number4)));     // 22
+
+
+
+// 2.Converting into the number 
+console.log("2.Converting into the String");
+
+
+
+ name="rukmoddin";  // String
+ number1="22"       // Number
+ number2="22a"      // String
+ number3=true;      // Boolean
+  number4=22;        //Number 
+
+
+
+console.log(String(name));         
+console.log(String(number1));     
+console.log(String(number2));     
+console.log(String(number3));     
+console.log(String(number4));     // 22
+
+
+
+console.log(typeof(String(name)));
+console.log(typeof(String(number1)));
+console.log(typeof(String(number2)));
+console.log(typeof(String(number3)));
+console.log(typeof(String(number4)));     // 22
+
+
+// 3.Converting into the Boolean 
+console.log("3.Converting into the Boolean");
+
+
+name="rukmoddin";  // String..
+number1="22"       // Number
+number2="22a"      // String
+number3=true;      // Boolean
+ number4=22;        //Number 
+
+
+
+console.log(Boolean(name));        
+console.log(Boolean(number1));     
+console.log(Boolean(number2));     
+console.log(Boolean(number3));     
+console.log(Boolean(number4));     // 22
+
+
+
+console.log(typeof(Boolean(name)));
+console.log(typeof(Boolean(number1)));
+console.log(typeof(Boolean(number2)));
+console.log(typeof(Boolean(number3)));
+console.log(typeof(Boolean(number4)));     // 22
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
