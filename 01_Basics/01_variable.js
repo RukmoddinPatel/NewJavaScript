0cce463a4b224a418bc23f5058b56ecd1f6dd7fb
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-08-26 22:51:36 +0530

    Variable

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,18 @@
+const accountId=12334;
+let accountEmail="rukmoddinpatel@123"
+var accountCity="Maindargi";
+Name="Rukmoddin Nabab Patel";
+
+console.log(accountId);
+
+
+/*
+Note : 
+1.If declared variable with const then its values would't be changed
+2.Don't use the var in ur life because of functional and block scop
+*/
+
+
+
+
+
