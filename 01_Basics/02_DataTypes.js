1eb1c98c7d1a3bce4c798e8ab2d8aa3011de2853
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-08-26 22:47:07 +0530

    Data Types

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,27 @@
+"use strict ";//treat all js code as newer version 
+
+// alert(3+3) we are using node js not browser 
+
+console.log(3+3);
+console.log("Rukmoddin Nabab Patel");
+
+
+
+// Data types
+//1. number
+//2. bigint
+//3.string=>" " or ' ' both will work 
+//4.null
+//5.undefined 
+//6.symbol => unique 
+
+
+console.log(typeof(null));  // Object 
+console.log(typeof(undefined));  // Undefined 
+
+
+
+
+
+
+
