aee1d8662c7c969b4ae92dfd77d39f9edc444590
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-08-27 17:51:45 +0530

    String Operations

---
+++ 01_Basics/05_Strings.js
@@ -0,0 +1,27 @@
+
+// Common Methods
+const commonMethod="Rukmoddin Nabab Patel";
+console.log("Rukmoddin"+" Nabab"+" Patel");
+
+// Modern Method
+Name="Rukmddin";
+Age=19
+console.log(`Hello jee i am ${Name} i am ${Age} year old Boy`);
+
+// One more method
+const urName=new String('Rukmoddin');
+console.log(urName);
+
+console.log(urName.charAt(2));
+console.log(urName.indexOf('i'));
+
+const subString=urName.substring(0,9);
+console.log(subString);
+
+const slice=urName.slice()
+
+
+
+
+
+
