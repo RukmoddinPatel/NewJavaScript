ea6a807248de8f23b9f7f1c25b0d0ef33da283cb
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-08-27 23:26:41 +0530

    Nums and Maths

---
+++ 01_Basics/06_nums_n_Maths.js
@@ -0,0 +1,37 @@
+const num=120.342;
+
+const balance=new Number(100);
+console.log(balance);
+
+console.log(num.toFixed(10));
+console.log(num.toPrecision(3));
+
+
+
+
+// +++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min=10;
+const max=20;
+
+
+// If u have to genearate random the value between two limit
+console.log(Math.floor(Math.random()*(max-min+1)+min));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
