b464b330a3660ed26dce5367674807c692b86b96
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-08-27 17:00:40 +0530

    DataTypes Summary

---
+++ 01_Basics/data_types_Summary.js
@@ -0,0 +1,48 @@
+// Primitive data types 
+// 7 types : String ,Number ,Boolean ,null ,undefinde,symbol,BigInt
+
+
+// Number 
+const score=100
+const scores=100.12
+
+// String
+const name="Rukmdodin";
+
+// Boolean
+const isLoggedIn=false;
+
+// null
+const outsideTemp=null;
+
+ //undefied
+let userEmail;
+
+// Symbol
+const id1=Symbol('123')
+const id2=Symbol('123')
+
+// BigInt
+const n=12345678901234567899n;
+
+
+
+
+
+// Reference Type (Non Primitive)
+//  Array ,Objects,Functions 
+
+
+const arr=["Hatim","Ironman","Spiderman","Rukmoddin"]
+
+let myObjs={
+    name:"hitesh",
+    aga:22,
+}
+
+
+
+const myFunct=function(){
+    console.log("Hello I am Rukmoddin Nabab Patel")
+}
+
