22f5e7ec7d159a24fbbd2b05bbbe14484f321845
RukmoddinPatel <rukmoddinpatel27@gmail.com>
2024-08-29 15:40:44 +0530

    Object Destructuring

---
+++ 02_Basics/03_Objects.js
@@ -0,0 +1,61 @@
+
+
+const myObj={
+    name:"Rukmoddin Nabab Patel",
+    age:19,
+    weight:58,
+    city:"pune",
+    Occupation:"Student",
+    Branch:"Computer Engineering"
+}
+
+// U can fetch the value by '.' but prefer [] index becoz in some case dot fetching won't work
+console.log(myObj.name);
+console.log(myObj["name"]);
+
+
+JsUser.email="hiteshChoudhary@"
+
+
+// Object destructuring 
+
+const myJavaScriptCourse={
+    mentor:"Hitesh Sir",
+    platform:"U-Tube",
+    Language:"Js",
+    vdo_Count:50
+}
+
+
+// Destructuring the object
+// if u r want multiple to print many times then u can use the 
+
+const {mentor}=myJavaScriptCourse
+console.log(mentor);
+
+
+// API : JSON or just like an object 
+
+{
+
+    "name":"Rukmoddin Nabab Patel",
+    "city":"pune",
+    "age":19,
+    "hobby":"Confuse Rahena",
+    "Branch":"CS"
+
+
+}
+
+
+[
+    {},
+    {},
+    {},
+    {}
+]
+
+
+
+
+
